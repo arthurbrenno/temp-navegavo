09feef6b417c1ec51ae977eb775e9652877f3e61
Arthur <arthurbreno009@gmail.com>
2024-09-12 23:53:18 +0000

    Update navegavo/controllers.py

--- navegavo/controllers.py
+++ navegavo/controllers.py
@@ -1,14 +1,14 @@
 import typing
 
-from litestar import Controller, Litestar, MediaType, post
-from litestar.datastructures import UploadFile
+from litestar import Controller, MediaType, post
 from litestar.di import Provide
 from litestar.enums import RequestEncodingType
 from litestar.params import Body, Dependency
+from llama_index.core.base.llms.types import ChatResponse
 from llama_index.core.multi_modal_llms.base import MultiModalLLM
 
 from .di import llm_factory
-from .schema import ScreenInfo
+from .schema import ScreenInfoRequest, ScreenInfoResponse
 
 
 class NavegavoController(Controller):
@@ -19,10 +19,9 @@ class NavegavoController(Controller):
     async def get_screen_info(
         self,
         data: typing.Annotated[
-            UploadFile, Body(media_type=RequestEncodingType.MULTI_PART)
+            ScreenInfoRequest, Body(media_type=RequestEncodingType.MULTI_PART)
         ],
-        llm: typing.Annotated[MultiModalLLM, Dependency(skip_validation=True)]
-    ) -> ScreenInfo:
-        contents: bytes = await data.read()
-        llm.
-        return ScreenInfo(description="Temp.")
+        llm: typing.Annotated[MultiModalLLM, Dependency(skip_validation=True)],
+    ) -> ScreenInfoResponse:
+        response: ChatResponse = await llm.achat(messages=data.messages)
+        return ScreenInfoResponse(response=response)
