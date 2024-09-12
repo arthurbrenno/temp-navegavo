510aaf50b68d33229ff165f532d7788c316e8b3a
Arthur <arthurbreno009@gmail.com>
2024-09-12 23:55:39 +0000

    Update navegavo/di.py

--- navegavo/di.py
+++ navegavo/di.py
@@ -1,6 +1,6 @@
 from llama_index.core.multi_modal_llms.base import MultiModalLLM
-from llama_index.core.multi_modal_llms.generic_utils import load_image_urls
-from llama_index.multi_modal_llms.gemini import GeminiMultiModal # type: ignore
+from llama_index.multi_modal_llms.gemini import GeminiMultiModal  # type: ignore
+
 
 async def llm_factory() -> MultiModalLLM:
     return GeminiMultiModal()
