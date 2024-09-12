904fd066ad180392860c83e4a31ce61a2ae2aca1
Arthur <arthurbreno009@gmail.com>
2024-09-12 23:53:18 +0000

    Update navegavo/__main__.py

--- navegavo/__main__.py
+++ navegavo/__main__.py
@@ -1,11 +1,13 @@
 if __name__ == "__main__":
     import uvicorn
     from .logging import logger
+    import os
 
     config = uvicorn.Config(app="navegavo.asgi:app", host="0.0.0.0", port=8000)
     server = uvicorn.Server(config=config)
 
     try:
+        os.system("clear" if os.name != "nt" else "cls")
         server.run()
     except Exception as e:
         logger.exception(e)
