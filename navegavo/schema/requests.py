46a2088a340ba6ddeb1485bd3020e546bafdf32e
Arthur <arthurbreno009@gmail.com>
2024-09-12 23:53:18 +0000

    Update navegavo/schema/requests.py

--- navegavo/schema/requests.py
+++ navegavo/schema/requests.py
@@ -2,8 +2,17 @@ import msgspec
 import typing
 from llama_index.core.base.llms.types import ChatMessage
 
-class ScreenHelpRequest(msgspec.Struct):
+
+class ScreenInfoRequest(msgspec.Struct):
     messages: typing.Annotated[
         typing.List[ChatMessage],
-        msgspec.Meta(title="", description="")
+        msgspec.Meta(title="Messages", description="A list of messages to chat with"),
+    ]
+
+    b64_image: typing.Annotated[
+        typing.Optional[str],
+        msgspec.Meta(
+            title="Base 64 Encoded Image",
+            description="The base 64 encoded image to send to the LLM",
+        ),
     ]
