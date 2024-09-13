bf260a895cbea450ce69124036a72888b98d8c9c
Arthur <arthurbreno009@gmail.com>
2024-09-13 22:20:53 +0000

    Update navegavo/schema/__init__.py

--- navegavo/schema/__init__.py
+++
@@ -1,6 +0,0 @@
-from __future__ import annotations
-
-from .requests import ScreenInfoRequest
-from .responses import ScreenInfoResponse
-
-__all__: list = ["ScreenInfoRequest", "ScreenInfoResponse"]
