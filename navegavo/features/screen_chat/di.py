9a4fbfcd68009014265a22997a60599ba7adbcae
Arthur Brenno <64020210+arthurbrenno@users.noreply.github.com>
2024-09-14 20:00:33 -0300

    Update navegavo/features/screen_chat/di.py

--- navegavo/features/screen_chat/di.py
+++ navegavo/features/screen_chat/di.py
@@ -1,17 +1,11 @@
 import typing
 
-from llama_index.core.base.llms.types import ChatResponse
-from llama_index.core.multi_modal_llms.base import MultiModalLLM
-from llama_index.multi_modal_llms.gemini import GeminiMultiModal  # type: ignore
+from openai.types.completion import Completion
 
 from navegavo.base import AsyncService
 
 from .services import ScreenChatService
 
 
-async def llm_factory() -> MultiModalLLM:
-    return GeminiMultiModal()
-
-
-async def service_cls_factory() -> typing.Type[AsyncService[ChatResponse]]:
+async def service_cls_factory() -> typing.Type[AsyncService[Completion]]:
     return ScreenChatService
