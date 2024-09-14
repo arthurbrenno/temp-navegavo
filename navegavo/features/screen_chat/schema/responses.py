cad7b42cf159fbb777e189bd20944f3ad2388acc
Arthur Brenno <64020210+arthurbrenno@users.noreply.github.com>
2024-09-14 20:00:33 -0300

    Update navegavo/features/screen_chat/schema/responses.py

--- navegavo/features/screen_chat/schema/responses.py
+++ navegavo/features/screen_chat/schema/responses.py
@@ -2,11 +2,11 @@ from __future__ import annotations
 
 import msgspec
 import typing
-from llama_index.core.base.llms.types import ChatResponse
+from openai.types.completion import Completion
 
 
 class ScreenInfoResponse(msgspec.Struct):
     response: typing.Annotated[
-        ChatResponse,
+        Completion,
         msgspec.Meta(title="Response", description="The response from the LLM."),
     ]
