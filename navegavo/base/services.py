9c928960ccca48ea6dc2b8c2d4cdd0fc8ec101e8
Arthur Brenno <64020210+arthurbrenno@users.noreply.github.com>
2024-09-14 21:10:30 -0300

    Update navegavo/base/services.py

--- navegavo/base/services.py
+++
@@ -1,8 +0,0 @@
-import typing
-
-from navegavo.typing import T_co
-
-
-@typing.runtime_checkable
-class AsyncService(typing.Protocol, typing.Generic[T_co]):
-    async def execute(self) -> T_co: ...
