851cafbf29e9d9cded902c00fb09a85fc65c7bec
Arthur Brenno <64020210+arthurbrenno@users.noreply.github.com>
2024-09-14 21:10:30 -0300

    Update navegavo/features/screen_chat/di.py

--- navegavo/features/screen_chat/di.py
+++ navegavo/features/screen_chat/di.py
@@ -1,11 +1,6 @@
 import typing
-
-from openai.types.completion import Completion
-
-from navegavo.base import AsyncService
-
 from .services import ScreenChatService
 
 
-async def service_cls_factory() -> typing.Type[AsyncService[Completion]]:
+async def service_cls_factory() -> typing.Type[ScreenChatService]:
     return ScreenChatService
