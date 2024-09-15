1b420b80447999a5f30e7639c28764d9d64949ef
Arthur Brenno <64020210+arthurbrenno@users.noreply.github.com>
2024-09-14 21:06:31 -0300

    Update navegavo/features/screen_chat/schema/requests.py

--- navegavo/features/screen_chat/schema/requests.py
+++
@@ -1,9 +0,0 @@
-import msgspec
-import typing
-
-
-class ScreenInfoRequest(msgspec.Struct):
-    messages: typing.Annotated[
-        typing.List[typing.Dict[str, typing.Any]],
-        msgspec.Meta(title="Messages", description="A list of messages to chat with"),
-    ]
